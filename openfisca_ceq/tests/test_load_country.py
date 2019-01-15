afb87ca07679319bf94215880300b8e5ac8471a3
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-01-15 23:05:49 +0100

    WIP

--- openfisca_ceq/tests/test_load_country.py
+++ openfisca_ceq/tests/test_load_country.py
@@ -5,7 +5,10 @@ import os
 import pkg_resources
 
 
-from openfisca_ceq import entities
+from openfisca_ceq import (
+    CountryTaxBenefitSystem as CEQTaxBenefitSystem,
+    entities,
+    )
 
 
 ceq_variables_directory = os.path.join(
@@ -15,29 +18,17 @@ ceq_variables_directory = os.path.join(
     )
 assert os.path.exists(ceq_variables_directory)
 
-
-from openfisca_ceq import CountryTaxBenefitSystem as CEQTaxBenefitSystem
-
-ceq_variables = CEQTaxBenefitSystem().variables
-print(ceq_variables.keys())
-
-print(ceq_variables['disposable_income'].formulas)
-print(ceq_variables['property_tax'].formulas)
-
-
-ceq_input_variables = [
+ceq_input_variables = {
     name
     for name, variable in CEQTaxBenefitSystem().variables.items()
     if len(variable.formulas) == 0
-    ]
+    }
 
-ceq_computed_variables = [
+ceq_computed_variables = {
     name
     for name, variable in CEQTaxBenefitSystem().variables.items()
     if len(variable.formulas) > 0
-    ]
-
-print(ceq_computed_variables)
+    }
 
 
 def add_ceq(country_tax_benefit_system):
@@ -45,13 +36,21 @@ def add_ceq(country_tax_benefit_system):
     entities_by_name = dict((entity.key, entity) for entity in country_entities)
     entities.Person = entities_by_name['person']
     entities.Household = entities_by_name['household']
-
-    country_variables = country_tax_benefit_system.variables.keys()
-
+    country_variables = set(country_tax_benefit_system.variables.keys())
     print("Missing CEQ input variables:\n{}".format(
-        set(ceq_input_variables).difference(set(country_variables)))
-        )
+        " - " + ('\n - ').join(
+            list(sorted(
+                ceq_input_variables.difference(country_variables)
+                ))
+            )
+        ))
+    assert not country_variables.intersection(ceq_computed_variables), \
+        "Some country variables matches computed CEQ variables: {}".format(
+            country_variables.intersection(ceq_computed_variables))
+
+    # TODO load only variables that do not already exist
     country_tax_benefit_system.add_variables_from_directory(ceq_variables_directory)
+
     return country_tax_benefit_system
 
 
@@ -60,8 +59,6 @@ if __name__ == '__main__':
     from openfisca_cote_d_ivoire.survey_scenarios import CoteDIvoireSurveyScenario
     country_tax_benefit_system = CountryTaxBenefitSystem()
     new_tbs = add_ceq(country_tax_benefit_system)
-
-
     from openfisca_cote_d_ivoire.tests.test_survey_scenario_from_stata_data import create_data_from_stata
     data = create_data_from_stata()
     survey_scenario = CoteDIvoireSurveyScenario(
