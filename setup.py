0ff2370ee41360733ac5720e4482bc55b28aa6e4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-08-21 17:08:28 +0200

    Inverse dep install order

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     url='https://github.com/openfisca/openfisca-ceq',
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        'OpenFisca-Core >=34.3.2,<35.0',
         'OpenFisca-Survey-Manager >=0.35,<1.0',
+        'OpenFisca-Core >=34.3.2,<35.0',
         'pandas >= 0.24.1',
         'python-slugify',
         'xlrd >= 1.0.0',
