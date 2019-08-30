cca2ce00d188dd12ee55ddcaf49acdf0312a23ce
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-08-30 19:11:41 +0200

    WIP

--- openfisca_ceq/tools/expenditures_loader.py
+++ openfisca_ceq/tools/expenditures_loader.py
@@ -12,8 +12,8 @@ log = logging.getLogger(__name__)
 config_parser = configparser.ConfigParser()
 config_parser.read(os.path.join(config_files_directory, 'raw_data.ini'))
 
-# expenditures_data_path = config_parser.get('mali', 'consommation_2014')
-expenditures_data_path = config_parser.get('senegal', 'consommation_2011')
+expenditures_data_path = config_parser.get('mali', 'consommation_2014')
+# expenditures_data_path = config_parser.get('senegal', 'consommation_2011')
 
 expenditures = pd.read_stata(expenditures_data_path)
 
--- openfisca_ceq/tools/income_loader.py
+++ openfisca_ceq/tools/income_loader.py
@@ -29,11 +29,10 @@ for d in [non_ceq_input_by_person_variable, ceq_intermediate_by_person_variable,
 print(income_data_path)
 
 income = pd.read_stata(income_data_path)
+for var in income.columns:
+    if var.startswith("rev"):
+        print(var)
 
 assert set(model_variable_by_person_variable.keys()) <= set(income.columns), "Missing {} in income data source".format(
     set(model_variable_by_person_variable.keys()).difference(set(income.columns))
     )
-
-print(income.columns)
-for var in income.columns:
-    print(income[var].describe())
