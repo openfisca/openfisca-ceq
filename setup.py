00fb3a30105ef513f6d465dcd96b38a8896016c0
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-02-12 00:14:42 +0100

    Try to rebuild

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OpenFisca-CEQ',
-    version='0.3.0',
+    version='0.3.1',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     description=u'OpenFisca tax and benefit system for CEQ',
